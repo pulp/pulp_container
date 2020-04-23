97ea4c4dbc592aa7b3e59b593bfebc8910e5ce4a
Ina Panova <ipanova@redhat.com>
2020-04-23 17:40:42 +0200

    Bump version to 1.3.1.dev.
    [noissue]

--- pulp_container/__init__.py
+++ pulp_container/__init__.py
@@ -1,3 +1,3 @@
-__version__ = '1.3.0'
+__version__ = '1.3.1.dev'
 
 default_app_config = 'pulp_container.app.PulpContainerPluginAppConfig'
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open('README.rst') as f:
 
 setup(
     name='pulp-container',
-    version='1.3.0',
+    version='1.3.1.dev',
     description='Container plugin for the Pulp Project',
     long_description=long_description,
     license='GPLv2+',
