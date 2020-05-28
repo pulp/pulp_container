b3cca43c96d64c48f142a7eeef141858de27a973
Ina Panova <ipanova@redhat.com>
2020-05-28 19:46:56 +0200

    Bump to 1.5.0.dev
    [noissue]

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.rst') as f:
 
 setup(
     name='pulp-container',
-    version='1.4.0',
+    version='1.5.0.dev',
     description='Container plugin for the Pulp Project',
     long_description=long_description,
     license='GPLv2+',
