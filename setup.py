ca256bb7aa1268acfa0934e131206e53a31b1108
Ina Panova <ipanova@redhat.com>
2020-06-08 18:30:14 +0200

    Releasing 2.0.0b2
    [noissue]

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.rst') as f:
 
 setup(
     name='pulp-container',
-    version='2.0.0b2.dev',
+    version='2.0.0b2',
     description='Container plugin for the Pulp Project',
     long_description=long_description,
     license='GPLv2+',
