e51a22baecec15025641cdd9695ae8ded74bc647
Matthias Dellweg <mdellweg@redhat.com>
2020-07-13 17:30:14 +0200

    Bump to 1.4.3.dev
    [noissue]

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.rst') as f:
 
 setup(
     name='pulp-container',
-    version='1.4.2',
+    version='1.4.3.dev',
     description='Container plugin for the Pulp Project',
     long_description=long_description,
     license='GPLv2+',
