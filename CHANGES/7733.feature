5df2058112abadfdbe7380b1db977cb93f9bef4f
Matthias Dellweg <mdellweg@redhat.com>
2020-10-21 11:56:04 +0200

    Allow container build with django-storage
    This isolates the build context in another subdirectory
    
    fixes #7733
    https://pulp.plan.io/issues/7733

---
+++ CHANGES/7733.feature
@@ -0,0 +1,3 @@
+Enabled the container build feature to work with django-storage.
+
+Isolated the build context in a subdirectory to avoid name collisions.
--- pulp_container/app/tasks/builder.py
+++ pulp_container/app/tasks/builder.py
@@ -32,7 +32,7 @@ def get_or_create_blob(layer_json, manifest, path):
     try:
         blob = Blob.objects.get(digest=layer_json["digest"])
     except Blob.DoesNotExist:
-        layer_file_name = "{}{}".format(path, layer_json["digest"][7:])
+        layer_file_name = os.path.join(path, layer_json["digest"][7:])
         layer_artifact = Artifact.init_and_validate(layer_file_name)
         layer_artifact.save()
         blob = Blob(digest=layer_json["digest"], media_type=layer_json["mediaType"])
@@ -59,7 +59,7 @@ def add_image_from_directory_to_repository(path, repository, tag):
         image and tag.
 
     """
-    manifest_path = "{}manifest.json".format(path)
+    manifest_path = os.path.join(path, "manifest.json")
     manifest_artifact = Artifact.init_and_validate(manifest_path)
     manifest_artifact.save()
     manifest_digest = "sha256:{}".format(manifest_artifact.sha256)
@@ -76,15 +76,14 @@ def add_image_from_directory_to_repository(path, repository, tag):
     with repository.new_version() as new_repo_version:
         new_repo_version.add_content(Manifest.objects.filter(pk=manifest.pk))
         new_repo_version.add_content(Tag.objects.filter(pk=tag.pk))
-        with open(manifest_artifact.file.path, "r") as manifest_file:
-            manifest_json = json.load(manifest_file)
-            config_blob = get_or_create_blob(manifest_json["config"], manifest, path)
-            manifest.config_blob = config_blob
-            manifest.save()
-            new_repo_version.add_content(Blob.objects.filter(pk=config_blob.pk))
-            for layer in manifest_json["layers"]:
-                blob = get_or_create_blob(layer, manifest, path)
-                new_repo_version.add_content(Blob.objects.filter(pk=blob.pk))
+        manifest_json = json.load(manifest_artifact.file)
+        config_blob = get_or_create_blob(manifest_json["config"], manifest, path)
+        manifest.config_blob = config_blob
+        manifest.save()
+        new_repo_version.add_content(Blob.objects.filter(pk=config_blob.pk))
+        for layer in manifest_json["layers"]:
+            blob = get_or_create_blob(layer, manifest, path)
+            new_repo_version.add_content(Blob.objects.filter(pk=blob.pk))
     return new_repo_version
 
 
@@ -116,25 +115,32 @@ def build_image_from_containerfile(
     with WorkingDirectory() as working_directory:
         buildah_env = os.environ.copy()
         buildah_env.update(
-            {"_BUILDAH_STARTED_IN_USERNS": "", "BUILDAH_ISOLATION": "chroot", "HOME": "/"}
+            {
+                "HOME": working_directory.path,
+            }
         )
-        path = "{}/".format(working_directory.path)
+        context_path = os.path.join(working_directory.path, "context")
+        os.makedirs(context_path)
         for key, val in artifacts.items():
             artifact = Artifact.objects.get(pk=key)
-            dirs = os.path.split(val)[0]
+            dest_path = os.path.join(context_path, val)
+            dirs = os.path.split(dest_path)[0]
             if dirs:
                 os.makedirs(dirs)
-
-            shutil.copy(artifact.file.path, "{}{}".format(path, val))
+            with open(dest_path, "wb") as dest:
+                shutil.copyfileobj(artifact.file, dest)
+        containerfile_path = os.path.join(working_directory.path, "Containerfile")
+        with open(containerfile_path, "wb") as dest:
+            shutil.copyfileobj(containerfile.file, dest)
         bud_cp = subprocess.run(
-            ["buildah", "bud", "-f", containerfile.file.path, "-t", name, path],
+            ["buildah", "bud", "-f", containerfile_path, "-t", name, context_path],
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
             env=buildah_env,
         )
         if bud_cp.returncode != 0:
             raise Exception(bud_cp.stderr)
-        image_dir = "{}image/".format(path)
+        image_dir = os.path.join(working_directory.path, "image")
         os.makedirs(image_dir)
         push_cp = subprocess.run(
             ["buildah", "push", "-f", "oci", name, "dir:{}".format(image_dir)],
