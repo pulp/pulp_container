5541586cdd1c50c69a12f54bb197460a4ec09bca
Ina Panova <ipanova@redhat.com>
2020-11-23 19:17:05 +0100

    Remove travis badge and make CI adjustments.
    [noissue]
    
    Added pulp.example.com to the /etc/hosts to fix pull operation.
    Fixed docs scripts.

--- docs/_scripts/image_untagging.sh
+++ docs/_scripts/image_untagging.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
-export TAG_NAME='custom_tag'
+TAG_NAME='custom_tag'
 
 echo "Untagging a manifest which is labeled with ${TAG_NAME}"
-export TASK_URL=$(http POST $BASE_ADDR$REPO_HREF'untag/' tag=$TAG_NAME \
+TASK_URL=$(http POST $BASE_ADDR$REPO_HREF'untag/' tag=$TAG_NAME \
   | jq -r '.task')
 
 wait_until_task_finished $BASE_ADDR$TASK_URL
 
 echo "Getting a reference to all removed tags."
-export REPO_VERSION=$(http $BASE_ADDR$TASK_URL \
+REPO_VERSION=$(http $BASE_ADDR$TASK_URL \
   | jq -r '.created_resources | first')
-export REMOVED_TAGS=$(http $BASE_ADDR$REPO_VERSION \
+REMOVED_TAGS=$(http $BASE_ADDR$REPO_VERSION \
   | jq -r '.content_summary | .removed | ."container.tag" | .href')
 
 echo "List removed tags from the latest repository version."
--- docs/_scripts/repo.sh
+++ docs/_scripts/repo.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-export REPO_NAME=$(head /dev/urandom | tr -dc a-z | head -c5)
+REPO_NAME=$(head /dev/urandom | tr -dc a-z | head -c5)
 
 echo "Creating a new repository named $REPO_NAME."
-export REPO_HREF=$(http POST $BASE_ADDR/pulp/api/v3/repositories/container/container/ name=$REPO_NAME \
+REPO_HREF=$(http POST $BASE_ADDR/pulp/api/v3/repositories/container/container/ name=$REPO_NAME \
   | jq -r '.pulp_href')
 
 echo "Inspecting repository."
