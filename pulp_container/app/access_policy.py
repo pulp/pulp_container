0565b0acd819cd7f31df8a41460038625fe27534
Ina Panova <ipanova@redhat.com>
2020-12-22 17:48:52 +0100

    Add manage_namespace_distributions permissions.
    [noissue]

--- pulp_container/app/access_policy.py
+++ pulp_container/app/access_policy.py
@@ -8,23 +8,24 @@ class DistributionAccessPolicyFromDB(AccessPolicyFromDB):
     Access policy for DistributionViewSet which handles namespace permissions.
     """
 
-    def has_add_namespace_perms(self, request, view, action):
+    def has_manage_namespace_dist_perms(self, request, view, action, permission):
         """
-        Check whether a user can create a namespace or can read the namespace if it already exists.
+        Check whether a user can create a namespace or it can manage distributions
+        for existing namespace.
         """
         namespace = request.data["base_path"].split("/")[0]
         try:
             namespace = models.ContainerNamespace.objects.get(name=namespace)
         except models.ContainerNamespace.DoesNotExist:
+            # check model permissions for namespace creation
             return request.user.has_perm("container.add_containernamespace")
         else:
-            return request.user.has_perm(
-                "container.view_containernamespace"
-            ) or request.user.has_perm("container.view_containernamespace", namespace)
+            # existing namespace
+            return request.user.has_perm(permission) or request.user.has_perm(permission, namespace)
 
-    def has_view_namespace_perms(self, request, view, action):
+    def has_namespace_obj_perms(self, request, view, action, permission):
         """
-        Test if a user is able read the namespace contained within a distribution.
+        Check if a user has object-level perms on the namespace associated with the distribution.
         """
         namespace = view.get_object().namespace
-        return request.user.has_perm("container.view_containernamespace", namespace)
+        return request.user.has_perm(permission, namespace)
