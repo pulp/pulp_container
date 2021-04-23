61920f244756154662e201d02a8e624ff4b1bf4e
David Newswanger <dnewswan@redhat.com>
2021-04-23 09:51:41 -0400

    Allow users with namespace permissions ot update distributions.
    closes #8618

--- pulp_container/app/viewsets.py
+++ pulp_container/app/viewsets.py
@@ -1026,7 +1026,7 @@ class ContainerDistributionViewSet(DistributionViewSet):
                 "principal": "authenticated",
                 "effect": "allow",
                 "condition": [
-                    "has_model_or_obj_perms:container.change_containerdistribution",
+                    "has_namespace_or_obj_perms:container.change_containerdistribution",
                     "has_namespace_or_obj_perms:container.view_containerdistribution",
                 ],
             },
