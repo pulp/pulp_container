e9bbfecbfa63762273a687e4bcaf5900c98c3d97
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2021-07-27 11:08:46 -0300

    Update CI + enable https
    [noissue]

--- .github/workflows/scripts/post_docs_test.sh
+++ .github/workflows/scripts/post_docs_test.sh
@@ -3,7 +3,7 @@
 # make sure this script runs at the repo root
 cd "$(dirname "$(realpath -e "$0")")"/../../..
 
-export BASE_ADDR=http://pulp.example.com:80
+export BASE_ADDR=https://pulp.example.com:443
 
 cd docs/_scripts/
 bash ./docs_check.sh
