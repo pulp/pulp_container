b0231b24c939b48c12e72d805e5f46c50d17c7e0
Ina Panova <ipanova@redhat.com>
2021-11-10 12:54:24 +0100

    Use general_multi_delete from core.
    closes #9562
    https://pulp.plan.io/issues/9562

--- pulp_container/app/tasks/__init__.py
+++ pulp_container/app/tasks/__init__.py
@@ -1,4 +1,3 @@
-from .base import general_multi_delete  # noqa
 from .builder import build_image_from_containerfile  # noqa
 from .recursive_add import recursive_add_content  # noqa
 from .recursive_remove import recursive_remove_content  # noqa
