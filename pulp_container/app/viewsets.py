8d77154aadb1173cbd255ddd26922469ee4040a6
Ina Panova <ipanova@redhat.com>
2021-11-25 17:20:36 +0100

    Sign images
    closes #500

--- pulp_container/app/viewsets.py
+++ pulp_container/app/viewsets.py
@@ -458,6 +458,53 @@ class TagOperationsMixin:
         return OperationPostponedResponse(result, request)
 
 
+class SignOperationsMixin:
+    """Signing mixing for both types of the repos."""
+
+    @extend_schema(
+        description="Trigger an asynchronous task to sign content.",
+        summary="Sign images in the repo",
+        responses={202: AsyncOperationResponseSerializer},
+    )
+    @action(detail=True, methods=["post"], serializer_class=serializers.RepositorySignSerializer)
+    def sign(self, request, pk):
+        """
+        Signs manifests by tag in the repository.
+        """
+        repository = self.get_object()
+        serializer = serializers.RepositorySignSerializer(
+            data=request.data, context={"request": request, "repository_pk": pk}
+        )
+
+        # Validate synchronously to return 400 errors.
+        serializer.is_valid(raise_exception=True)
+        signing_service = serializer.validated_data.get(
+            "manifest_signing_service", repository.manifest_signing_service
+        )
+        future_base_path = serializer.validated_data.get("future_base_path")
+        reference = f"{request.get_host()}/{future_base_path}"
+
+        tags_list = serializer.validated_data.get("tags_list")
+        if tags_list:
+            tags_list_pks = models.Tag.objects.filter(name__in=tags_list).values_list(
+                "pk", flat=True
+            )
+            tags_list_pks = list(tags_list_pks)
+        else:
+            tags_list_pks = None
+        result = dispatch(
+            tasks.sign,
+            exclusive_resources=[repository],
+            kwargs={
+                "repository_pk": repository.pk,
+                "reference": reference,
+                "signing_service_pk": signing_service.pk,
+                "tags_list": tags_list_pks,
+            },
+        )
+        return OperationPostponedResponse(result, request)
+
+
 class RepositoryVersionQuerySetMixin:
     """
     A mixin which provides with a custom `get_queryset` method for repository version viewsets.
@@ -487,7 +534,7 @@ class RepositoryVersionQuerySetMixin:
         return qs
 
 
-class ContainerRepositoryViewSet(TagOperationsMixin, RepositoryViewSet):
+class ContainerRepositoryViewSet(TagOperationsMixin, SignOperationsMixin, RepositoryViewSet):
     """
     ViewSet for container repo.
     """
@@ -544,7 +591,7 @@ class ContainerRepositoryViewSet(TagOperationsMixin, RepositoryViewSet):
                 ],
             },
             {
-                "action": ["add", "remove", "tag", "untag", "copy_tags", "copy_manifests"],
+                "action": ["add", "remove", "tag", "untag", "copy_tags", "copy_manifests", "sign"],
                 "principal": "authenticated",
                 "effect": "allow",
                 "condition": [
@@ -872,7 +919,7 @@ class ContainerRepositoryVersionViewSet(RepositoryVersionQuerySetMixin, Reposito
 
 
 class ContainerPushRepositoryViewSet(
-    TagOperationsMixin, ReadOnlyRepositoryViewSet, AsyncUpdateMixin
+    TagOperationsMixin, SignOperationsMixin, ReadOnlyRepositoryViewSet, AsyncUpdateMixin
 ):
     """
     ViewSet for a container push repository.
@@ -901,7 +948,7 @@ class ContainerPushRepositoryViewSet(
                 "condition": "has_namespace_or_obj_perms:container.view_containerpushrepository",
             },
             {
-                "action": ["tag", "untag", "remove_image"],
+                "action": ["tag", "untag", "remove_image", "sign"],
                 "principal": "authenticated",
                 "effect": "allow",
                 "condition": [
