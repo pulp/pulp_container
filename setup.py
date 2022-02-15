3d50062fefb01c652188393234d5e92225a0a0b5
pulpbot <pulp-infra@redhat.com>
2022-02-15 17:07:12 +0000

    Release 2.10.1 GH Issues: 542,523,537
    [noissue]

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.rst") as f:
 
 setup(
     name="pulp-container",
-    version="2.10.1.dev",
+    version="2.10.1",
     description="Container plugin for the Pulp Project",
     long_description=long_description,
     license="GPLv2+",
