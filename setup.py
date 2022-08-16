ddfc17b95eeb7b1024d121b81003d13e7fbcc58c
pulpbot <pulp-infra@redhat.com>
2022-08-16 19:14:41 +0000

    Bump to 2.10.8.dev
    [noissue]

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.rst") as f:
 
 setup(
     name="pulp-container",
-    version="2.10.7",
+    version="2.10.8.dev",
     description="Container plugin for the Pulp Project",
     long_description=long_description,
     license="GPLv2+",
