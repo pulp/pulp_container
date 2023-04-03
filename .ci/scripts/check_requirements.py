pulp/pulp_deb

Update CI files
[noissue]

.ci/scripts/check_requirements.py
@@ -1,0 +1,62 @@
+# WARNING: DO NOT EDIT!
+#
+# This file was generated by plugin_template, and is managed by it. Please use
+# './plugin-template --github pulp_deb' to update this file.
+#
+# For more info visit https://github.com/pulp/plugin_template
+
+import warnings
+from pkg_resources import Requirement
+
+
+CHECK_MATRIX = [
+    ("requirements.txt", True, True, True),
+    ("dev_requirements.txt", False, True, False),
+    ("ci_requirements.txt", False, True, True),
+    ("doc_requirements.txt", False, True, False),
+    ("lint_requirements.txt", False, True, True),
+    ("unittest_requirements.txt", False, True, True),
+    ("functest_requirements.txt", False, True, True),
+    ("clitest_requirements.txt", False, True, True),
+]
+
+errors = []
+
+for filename, check_upperbound, check_prereleases, check_r in CHECK_MATRIX:
+    try:
+        with open(filename, "r") as fd:
+            for nr, line in enumerate(fd.readlines()):
+                line = line.strip()
+                if not line or line.startswith("#"):
+                    continue
+                try:
+                    req = Requirement.parse(line)
+                except ValueError:
+                    if line.startswith("git+"):
+                        # The single exception...
+                        if "pulp-smash" not in line:
+                            errors.append(f"{filename}:{nr}: Invalid source requirement: {line}")
+                    elif line.startswith("-r "):
+                        if check_r:
+                            errors.append(f"{filename}:{nr}: Invalid deferred requirement: {line}")
+                    else:
+                        errors.append(f"{filename}:{nr}: Unreadable requirement {line}")
+                else:
+                    if check_prereleases and req.specifier.prereleases:
+                        if req.name != "pulp-deb-client":
+                            errors.append(f"{filename}:{nr}: Prerelease versions found in {line}.")
+                    ops = [op for op, ver in req.specs]
+                    spec = str(req.specs)
+                    if "~=" in ops:
+                        warnings.warn(f"{filename}:{nr}: Please avoid using ~= on {req.name}!")
+                    elif "<" not in ops and "<=" not in ops and "==" not in ops:
+                        if check_upperbound:
+                            errors.append(f"{filename}:{nr}: Upper bound missing in {line}.")
+    except FileNotFoundError:
+        # skip this test for plugins that don't use this requirements.txt
+        pass
+
+if errors:
+    print("Dependency issues found:")
+    print("\n".join(errors))
+    exit(1)

.github/workflows/scripts/script.sh
@@ -8,11 +8,11 @@
 #
 # For more info visit https://github.com/pulp/plugin_template
 
+set -mveuo pipefail
+
 # make sure this script runs at the repo root
 cd "$(dirname "$(realpath -e "$0")")"/../../..
 REPO_ROOT="$PWD"
-
-set -mveuo pipefail
 
 source .github/workflows/scripts/utils.sh
 
@@ -44,21 +44,16 @@ if [[ "$TEST" = "docs" ]]; then
   exit
 fi
 
+REPORTED_STATUS="$(pulp status)"
+
 if [[ "${RELEASE_WORKFLOW:-false}" == "true" ]]; then
-  STATUS_ENDPOINT="${PULP_URL}${PULP_API_ROOT}api/v3/status/"
-  echo $STATUS_ENDPOINT
-  REPORTED_VERSION=$(http $STATUS_ENDPOINT | jq --arg plugin deb --arg legacy_plugin pulp_deb -r '.versions[] | select(.component == $plugin or .component == $legacy_plugin) | .version')
+  REPORTED_VERSION="$(echo $REPORTED_STATUS | jq --arg plugin deb --arg legacy_plugin pulp_deb -r '.versions[] | select(.component == $plugin or .component == $legacy_plugin) | .version')"
   response=$(curl --write-out %{http_code} --silent --output /dev/null https://pypi.org/project/pulp-deb/$REPORTED_VERSION/)
   if [ "$response" == "200" ];
   then
     echo "pulp_deb $REPORTED_VERSION has already been released. Skipping running tests."
     exit
   fi
-fi
-
-if [[ "$TEST" == "plugin-from-pypi" ]]; then
-  COMPONENT_VERSION=$(http https://pypi.org/pypi/pulp-deb/json | jq -r '.info.version')
-  git checkout ${COMPONENT_VERSION} -- pulp_deb/tests/
 fi
 
 echo "machine pulp
@@ -68,20 +63,36 @@ password password
 # Some commands like ansible-galaxy specifically require 600
 cmd_user_stdin_prefix bash -c "chmod 600 ~pulp/.netrc"
 
+cd ../pulp-openapi-generator
+if [ "$(echo "$REPORTED_STATUS" | jq -r '.versions[0].package')" = "null" ]
+then
+  # We are on an old version of pulpcore without package in the status report
+  for app_label in $(echo "$REPORTED_STATUS" | jq -r '.versions[].component')
+  do
+    if [ "$app_label" = "core" ]
+    then
+      item=pulpcore
+    else
+      item="pulp_${app_label}"
+    fi
+    ./generate.sh "${item}" python
+    cmd_prefix pip3 install "/root/pulp-openapi-generator/${item}-client"
+    sudo rm -rf "./${item}-client"
+  done
+else
+  for item in $(echo "$REPORTED_STATUS" | jq -r '.versions[].package|sub("-"; "_")')
+  do
+    ./generate.sh "${item}" python
+    cmd_prefix pip3 install "/root/pulp-openapi-generator/${item}-client"
+    sudo rm -rf "./${item}-client"
+  done
+fi
+
+cd $REPO_ROOT
+
 cat unittest_requirements.txt | cmd_stdin_prefix bash -c "cat > /tmp/unittest_requirements.txt"
 cat functest_requirements.txt | cmd_stdin_prefix bash -c "cat > /tmp/functest_requirements.txt"
-cmd_prefix pip3 install -r /tmp/unittest_requirements.txt
-cmd_prefix pip3 install -r /tmp/functest_requirements.txt
-cmd_prefix pip3 install --upgrade ../pulp-smash
-
-cd ../pulp-openapi-generator
-./generate.sh pulp_deb python
-cmd_prefix pip3 install /root/pulp-openapi-generator/pulp_deb-client
-sudo rm -rf ./pulp_deb-client
-./generate.sh pulpcore python
-cmd_prefix pip3 install /root/pulp-openapi-generator/pulpcore-client
-sudo rm -rf ./pulpcore-client
-cd $REPO_ROOT
+cmd_prefix pip3 install -r /tmp/unittest_requirements.txt -r /tmp/functest_requirements.txt
 
 CERTIFI=$(cmd_prefix python3 -c 'import certifi; print(certifi.where())')
 cmd_prefix bash -c "cat /etc/pulp/certs/pulp_webserver.crt  | tee -a "$CERTIFI" > /dev/null"

pulp/pulp_container

.ci/scripts/check_requirements.py
@@ -1,0 +1,62 @@
+# WARNING: DO NOT EDIT!
+#
+# This file was generated by plugin_template, and is managed by it. Please use
+# './plugin-template --github pulp_container' to update this file.
+#
+# For more info visit https://github.com/pulp/plugin_template
+
+import warnings
+from pkg_resources import Requirement
+
+
+CHECK_MATRIX = [
+    ("requirements.txt", True, True, True),
+    ("dev_requirements.txt", False, True, False),
+    ("ci_requirements.txt", False, True, True),
+    ("doc_requirements.txt", False, True, False),
+    ("lint_requirements.txt", False, True, True),
+    ("unittest_requirements.txt", False, True, True),
+    ("functest_requirements.txt", False, True, True),
+    ("clitest_requirements.txt", False, True, True),
+]
+
+errors = []
+
+for filename, check_upperbound, check_prereleases, check_r in CHECK_MATRIX:
+    try:
+        with open(filename, "r") as fd:
+            for nr, line in enumerate(fd.readlines()):
+                line = line.strip()
+                if not line or line.startswith("#"):
+                    continue
+                try:
+                    req = Requirement.parse(line)
+                except ValueError:
+                    if line.startswith("git+"):
+                        # The single exception...
+                        if "pulp-smash" not in line:
+                            errors.append(f"{filename}:{nr}: Invalid source requirement: {line}")
+                    elif line.startswith("-r "):
+                        if check_r:
+                            errors.append(f"{filename}:{nr}: Invalid deferred requirement: {line}")
+                    else:
+                        errors.append(f"{filename}:{nr}: Unreadable requirement {line}")
+                else:
+                    if check_prereleases and req.specifier.prereleases:
+                        if req.name != "pulp-container-client":
+                            errors.append(f"{filename}:{nr}: Prerelease versions found in {line}.")
+                    ops = [op for op, ver in req.specs]
+                    spec = str(req.specs)
+                    if "~=" in ops:
+                        warnings.warn(f"{filename}:{nr}: Please avoid using ~= on {req.name}!")
+                    elif "<" not in ops and "<=" not in ops and "==" not in ops:
+                        if check_upperbound:
+                            errors.append(f"{filename}:{nr}: Upper bound missing in {line}.")
+    except FileNotFoundError:
+        # skip this test for plugins that don't use this requirements.txt
+        pass
+
+if errors:
+    print("Dependency issues found:")
+    print("\n".join(errors))
+    exit(1)

.github/workflows/scripts/script.sh
@@ -8,11 +8,11 @@
 #
 # For more info visit https://github.com/pulp/plugin_template
 
+set -mveuo pipefail
+
 # make sure this script runs at the repo root
 cd "$(dirname "$(realpath -e "$0")")"/../../..
 REPO_ROOT="$PWD"
-
-set -mveuo pipefail
 
 source .github/workflows/scripts/utils.sh
 
@@ -44,21 +44,16 @@ if [[ "$TEST" = "docs" ]]; then
   exit
 fi
 
+REPORTED_STATUS="$(pulp status)"
+
 if [[ "${RELEASE_WORKFLOW:-false}" == "true" ]]; then
-  STATUS_ENDPOINT="${PULP_URL}${PULP_API_ROOT}api/v3/status/"
-  echo $STATUS_ENDPOINT
-  REPORTED_VERSION=$(http $STATUS_ENDPOINT | jq --arg plugin container --arg legacy_plugin pulp_container -r '.versions[] | select(.component == $plugin or .component == $legacy_plugin) | .version')
+  REPORTED_VERSION="$(echo $REPORTED_STATUS | jq --arg plugin container --arg legacy_plugin pulp_container -r '.versions[] | select(.component == $plugin or .component == $legacy_plugin) | .version')"
   response=$(curl --write-out %{http_code} --silent --output /dev/null https://pypi.org/project/pulp-container/$REPORTED_VERSION/)
   if [ "$response" == "200" ];
   then
     echo "pulp_container $REPORTED_VERSION has already been released. Skipping running tests."
     exit
   fi
-fi
-
-if [[ "$TEST" == "plugin-from-pypi" ]]; then
-  COMPONENT_VERSION=$(http https://pypi.org/pypi/pulp-container/json | jq -r '.info.version')
-  git checkout ${COMPONENT_VERSION} -- pulp_container/tests/
 fi
 
 echo "machine pulp
@@ -68,20 +63,36 @@ password password
 # Some commands like ansible-galaxy specifically require 600
 cmd_user_stdin_prefix bash -c "chmod 600 ~pulp/.netrc"
 
+cd ../pulp-openapi-generator
+if [ "$(echo "$REPORTED_STATUS" | jq -r '.versions[0].package')" = "null" ]
+then
+  # We are on an old version of pulpcore without package in the status report
+  for app_label in $(echo "$REPORTED_STATUS" | jq -r '.versions[].component')
+  do
+    if [ "$app_label" = "core" ]
+    then
+      item=pulpcore
+    else
+      item="pulp_${app_label}"
+    fi
+    ./generate.sh "${item}" python
+    cmd_prefix pip3 install "/root/pulp-openapi-generator/${item}-client"
+    sudo rm -rf "./${item}-client"
+  done
+else
+  for item in $(echo "$REPORTED_STATUS" | jq -r '.versions[].package|sub("-"; "_")')
+  do
+    ./generate.sh "${item}" python
+    cmd_prefix pip3 install "/root/pulp-openapi-generator/${item}-client"
+    sudo rm -rf "./${item}-client"
+  done
+fi
+
+cd $REPO_ROOT
+
 cat unittest_requirements.txt | cmd_stdin_prefix bash -c "cat > /tmp/unittest_requirements.txt"
 cat functest_requirements.txt | cmd_stdin_prefix bash -c "cat > /tmp/functest_requirements.txt"
-cmd_prefix pip3 install -r /tmp/unittest_requirements.txt
-cmd_prefix pip3 install -r /tmp/functest_requirements.txt
-cmd_prefix pip3 install --upgrade ../pulp-smash
-
-cd ../pulp-openapi-generator
-./generate.sh pulp_container python
-cmd_prefix pip3 install /root/pulp-openapi-generator/pulp_container-client
-sudo rm -rf ./pulp_container-client
-./generate.sh pulpcore python
-cmd_prefix pip3 install /root/pulp-openapi-generator/pulpcore-client
-sudo rm -rf ./pulpcore-client
-cd $REPO_ROOT
+cmd_prefix pip3 install -r /tmp/unittest_requirements.txt -r /tmp/functest_requirements.txt
 
 CERTIFI=$(cmd_prefix python3 -c 'import certifi; print(certifi.where())')
 cmd_prefix bash -c "cat /etc/pulp/certs/pulp_webserver.crt  | tee -a "$CERTIFI" > /dev/null"