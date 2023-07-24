f0bb2600f985ace37d6fec8282e8e3ddfe2fb3ed
pulpbot <pulp-infra@redhat.com>
2023-07-24 15:42:29 +0000

    Release 2.15.2 GH Issues: 1329,1286
    [noissue]

--- pulp_container/app/__init__.py
+++ pulp_container/app/__init__.py
@@ -6,5 +6,5 @@ class PulpContainerPluginAppConfig(PulpPluginAppConfig):
 
     name = "pulp_container.app"
     label = "container"
-    version = "2.15.2.dev"
+    version = "2.15.2"
     python_package_name = "pulp-container"
