156070da2b6f4d918824073f282e566a4ab684dc
Lubos Mjachky <lmjachky@redhat.com>
2023-08-02 14:24:06 +0200

    Fix flatpak settings for the S3 test runner
    The flatpak_index setting has to be explicitly set to false because of
    the global pulp settings definition.
    
    Enabling the indexing for the S3 test runner would cause
    pulp_container/tests/functional/api/test_flatpak.py to fail due to
    <https://gitlab.gnome.org/GNOME/libsoup/-/issues/357>.
    
    [noissue]
    
    (cherry picked from commit 0bfc32caae39441212cbb2cbe9516036ab8a5a64)

--- .github/workflows/scripts/install.sh
+++ .github/workflows/scripts/install.sh
@@ -88,7 +88,7 @@ if [ "$TEST" = "s3" ]; then
   sed -i -e '$a s3_test: true\
 minio_access_key: "'$MINIO_ACCESS_KEY'"\
 minio_secret_key: "'$MINIO_SECRET_KEY'"\
-pulp_scenario_settings: null\
+pulp_scenario_settings: {"flatpak_index": false}\
 ' vars/main.yaml
   export PULP_API_ROOT="/rerouted/djnd/"
 fi
