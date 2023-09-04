3d69b21f4cdb4e9b798d11051f30d39428349af3
Lubos Mjachky <lmjachky@redhat.com>
2023-09-04 19:14:41 +0200

    Fix the docs search bar
    [noissue]

--- docs/conf.py
+++ docs/conf.py
@@ -34,7 +34,7 @@ except ImportError:
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.extlinks']
+extensions = ['sphinx.ext.extlinks', 'sphinxcontrib.jquery']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
