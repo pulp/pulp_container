pulp/pulp_file

Update CI files
[noissue]

.github/workflows/scripts/install_python_client.sh
@@ -16,8 +16,6 @@ source .github/workflows/scripts/utils.sh
 
 export PULP_URL="${PULP_URL:-https://pulp}"
 
-pip install twine wheel
-
 REPORTED_STATUS="$(pulp status)"
 REPORTED_VERSION="$(echo "$REPORTED_STATUS" | jq --arg plugin "file" -r '.versions[] | select(.component == $plugin) | .version')"
 VERSION="$(echo "$REPORTED_VERSION" | python -c 'from packaging.version import Version; print(Version(input()))')"
@@ -28,8 +26,8 @@ rm -rf pulp_file-client
 pushd pulp_file-client
 python setup.py sdist bdist_wheel --python-tag py3
 
-twine check "dist/pulp_file_client-$VERSION-py3-none-any.whl" || exit 1
-twine check "dist/pulp_file-client-$VERSION.tar.gz" || exit 1
+twine check "dist/pulp_file_client-$VERSION-py3-none-any.whl"
+twine check "dist/pulp_file-client-$VERSION.tar.gz"
 
 cmd_prefix pip3 install "/root/pulp-openapi-generator/pulp_file-client/dist/pulp_file_client-${VERSION}-py3-none-any.whl"
 tar cvf ../../pulp_file/file-python-client.tar ./dist
@@ -39,6 +37,21 @@ find ./docs/* -exec sed -i 's/README//g' {} \;
 cp README.md docs/index.md
 sed -i 's/docs\///g' docs/index.md
 find ./docs/* -exec sed -i 's/\.md//g' {} \;
+
+cat >> mkdocs.yml << DOCSYAML
+---
+site_name: PulpFile Client
+site_description: File bindings
+site_author: Pulp Team
+site_url: https://docs.pulpproject.org/pulp_file_client/
+repo_name: pulp/pulp_file
+repo_url: https://github.com/pulp/pulp_file
+theme: readthedocs
+DOCSYAML
+
+# Building the bindings docs
+mkdocs build
+
 tar cvf ../../pulp_file/file-python-client-docs.tar ./docs
 popd
 popd

.github/workflows/scripts/publish_client_gem.sh
@@ -12,27 +12,24 @@ set -euv
 # make sure this script runs at the repo root
 cd "$(dirname "$(realpath -e "$0")")"/../../..
 
-
-mkdir ~/.gem || true
-touch ~/.gem/credentials
-echo "---
-:rubygems_api_key: $RUBYGEMS_API_KEY" > ~/.gem/credentials
-sudo chmod 600 ~/.gem/credentials
-
-export VERSION=$(ls pulp_file_client* | sed -rn 's/pulp_file_client-(.*)\.gem/\1/p')
+VERSION="$1"
 
 if [[ -z "$VERSION" ]]; then
-  echo "No client package found."
-  exit
+  echo "No version specified."
+  exit 1
 fi
 
-export response=$(curl --write-out %{http_code} --silent --output /dev/null https://rubygems.org/gems/pulp_file_client/versions/$VERSION)
+RESPONSE="$(curl --write-out '%{http_code}' --silent --output /dev/null "https://rubygems.org/gems/pulp_file_client/versions/$VERSION")"
 
-if [ "$response" == "200" ];
+if [ "$RESPONSE" == "200" ];
 then
   echo "pulp_file client $VERSION has already been released. Skipping."
   exit
 fi
 
-GEM_FILE="$(ls pulp_file_client-*)"
-gem push ${GEM_FILE}
+mkdir -p ~/.gem
+touch ~/.gem/credentials
+echo "---
+:rubygems_api_key: $RUBYGEMS_API_KEY" > ~/.gem/credentials
+sudo chmod 600 ~/.gem/credentials
+gem push "pulp_file_client-${VERSION}.gem"

.github/workflows/scripts/publish_plugin_pypi.sh
@@ -7,23 +7,27 @@
 #
 # For more info visit https://github.com/pulp/plugin_template
 
+set -euv
+
 # make sure this script runs at the repo root
 cd "$(dirname "$(realpath -e "$0")")"/../../..
 
-set -euv
+VERSION="$1"
 
-export response=$(curl --write-out %{http_code} --silent --output /dev/null https://pypi.org/project/pulp-file/$1/)
-if [ "$response" == "200" ];
+if [[ -z "$VERSION" ]]; then
+  echo "No version specified."
+  exit 1
+fi
+
+RESPONSE="$(curl --write-out '%{http_code}' --silent --output /dev/null "https://pypi.org/project/pulp-file/$VERSION/")"
+
+if [ "$RESPONSE" == "200" ];
 then
-  echo "pulp_file $1 has already been released. Skipping."
+  echo "pulp_file $VERSION has already been released. Skipping."
   exit
 fi
 
-pip install twine
-
-twine check dist/pulp_file-$1-py3-none-any.whl || exit 1
-twine check dist/pulp-file-$1.tar.gz || exit 1
-twine upload dist/pulp_file-$1-py3-none-any.whl -u pulp -p $PYPI_PASSWORD
-twine upload dist/pulp-file-$1.tar.gz -u pulp -p $PYPI_PASSWORD
-
-exit $?
+twine upload -u pulp -p "$PYPI_PASSWORD" \
+"dist/pulp_file-$VERSION-py3-none-any.whl" \
+"dist/pulp-file-$VERSION.tar.gz" \
+;

pulp/pulp_container

.github/workflows/scripts/install_python_client.sh
@@ -16,8 +16,6 @@ source .github/workflows/scripts/utils.sh
 
 export PULP_URL="${PULP_URL:-https://pulp}"
 
-pip install twine wheel
-
 REPORTED_STATUS="$(pulp status)"
 REPORTED_VERSION="$(echo "$REPORTED_STATUS" | jq --arg plugin "container" -r '.versions[] | select(.component == $plugin) | .version')"
 VERSION="$(echo "$REPORTED_VERSION" | python -c 'from packaging.version import Version; print(Version(input()))')"
@@ -28,8 +26,8 @@ rm -rf pulp_container-client
 pushd pulp_container-client
 python setup.py sdist bdist_wheel --python-tag py3
 
-twine check "dist/pulp_container_client-$VERSION-py3-none-any.whl" || exit 1
-twine check "dist/pulp_container-client-$VERSION.tar.gz" || exit 1
+twine check "dist/pulp_container_client-$VERSION-py3-none-any.whl"
+twine check "dist/pulp_container-client-$VERSION.tar.gz"
 
 cmd_prefix pip3 install "/root/pulp-openapi-generator/pulp_container-client/dist/pulp_container_client-${VERSION}-py3-none-any.whl"
 tar cvf ../../pulp_container/container-python-client.tar ./dist
@@ -39,6 +37,21 @@ find ./docs/* -exec sed -i 's/README//g' {} \;
 cp README.md docs/index.md
 sed -i 's/docs\///g' docs/index.md
 find ./docs/* -exec sed -i 's/\.md//g' {} \;
+
+cat >> mkdocs.yml << DOCSYAML
+---
+site_name: PulpContainer Client
+site_description: Container bindings
+site_author: Pulp Team
+site_url: https://docs.pulpproject.org/pulp_container_client/
+repo_name: pulp/pulp_container
+repo_url: https://github.com/pulp/pulp_container
+theme: readthedocs
+DOCSYAML
+
+# Building the bindings docs
+mkdocs build
+
 tar cvf ../../pulp_container/container-python-client-docs.tar ./docs
 popd
 popd

.github/workflows/scripts/publish_client_gem.sh
@@ -12,27 +12,24 @@ set -euv
 # make sure this script runs at the repo root
 cd "$(dirname "$(realpath -e "$0")")"/../../..
 
-
-mkdir ~/.gem || true
-touch ~/.gem/credentials
-echo "---
-:rubygems_api_key: $RUBYGEMS_API_KEY" > ~/.gem/credentials
-sudo chmod 600 ~/.gem/credentials
-
-export VERSION=$(ls pulp_container_client* | sed -rn 's/pulp_container_client-(.*)\.gem/\1/p')
+VERSION="$1"
 
 if [[ -z "$VERSION" ]]; then
-  echo "No client package found."
-  exit
+  echo "No version specified."
+  exit 1
 fi
 
-export response=$(curl --write-out %{http_code} --silent --output /dev/null https://rubygems.org/gems/pulp_container_client/versions/$VERSION)
+RESPONSE="$(curl --write-out '%{http_code}' --silent --output /dev/null "https://rubygems.org/gems/pulp_container_client/versions/$VERSION")"
 
-if [ "$response" == "200" ];
+if [ "$RESPONSE" == "200" ];
 then
   echo "pulp_container client $VERSION has already been released. Skipping."
   exit
 fi
 
-GEM_FILE="$(ls pulp_container_client-*)"
-gem push ${GEM_FILE}
+mkdir -p ~/.gem
+touch ~/.gem/credentials
+echo "---
+:rubygems_api_key: $RUBYGEMS_API_KEY" > ~/.gem/credentials
+sudo chmod 600 ~/.gem/credentials
+gem push "pulp_container_client-${VERSION}.gem"

.github/workflows/scripts/publish_plugin_pypi.sh
@@ -7,23 +7,27 @@
 #
 # For more info visit https://github.com/pulp/plugin_template
 
+set -euv
+
 # make sure this script runs at the repo root
 cd "$(dirname "$(realpath -e "$0")")"/../../..
 
-set -euv
+VERSION="$1"
 
-export response=$(curl --write-out %{http_code} --silent --output /dev/null https://pypi.org/project/pulp-container/$1/)
-if [ "$response" == "200" ];
+if [[ -z "$VERSION" ]]; then
+  echo "No version specified."
+  exit 1
+fi
+
+RESPONSE="$(curl --write-out '%{http_code}' --silent --output /dev/null "https://pypi.org/project/pulp-container/$VERSION/")"
+
+if [ "$RESPONSE" == "200" ];
 then
-  echo "pulp_container $1 has already been released. Skipping."
+  echo "pulp_container $VERSION has already been released. Skipping."
   exit
 fi
 
-pip install twine
-
-twine check dist/pulp_container-$1-py3-none-any.whl || exit 1
-twine check dist/pulp-container-$1.tar.gz || exit 1
-twine upload dist/pulp_container-$1-py3-none-any.whl -u pulp -p $PYPI_PASSWORD
-twine upload dist/pulp-container-$1.tar.gz -u pulp -p $PYPI_PASSWORD
-
-exit $?
+twine upload -u pulp -p "$PYPI_PASSWORD" \
+"dist/pulp_container-$VERSION-py3-none-any.whl" \
+"dist/pulp-container-$VERSION.tar.gz" \
+;