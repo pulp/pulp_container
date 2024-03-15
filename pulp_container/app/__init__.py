ed1f1e571c7f8b50ea9204325644581503a75c70
pulpbot <pulp-infra@redhat.com>
2024-03-15 13:40:42 +0000

    Bump version: 2.14.13 → 2.14.14.dev

--- pulp_container/app/__init__.py
+++ pulp_container/app/__init__.py
@@ -6,5 +6,5 @@ class PulpContainerPluginAppConfig(PulpPluginAppConfig):
 
     name = "pulp_container.app"
     label = "container"
-    version = "2.14.13"
+    version = "2.14.14.dev"
     python_package_name = "pulp-container"
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.rst") as f:
 
 setup(
     name="pulp-container",
-    version="2.14.13",
+    version="2.14.14.dev",
     description="Container plugin for the Pulp Project",
     long_description=long_description,
     license="GPLv2+",
