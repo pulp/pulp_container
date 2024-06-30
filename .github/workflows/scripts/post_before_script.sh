fed7f22e9a2704598f485cd867585df77717323e
pulpbot <pulp-infra@redhat.com>
2024-06-30 02:42:27 +0000

    Update CI files
    [noissue]

--- .github/workflows/scripts/post_before_script.sh
+++ .github/workflows/scripts/post_before_script.sh
@@ -3,3 +3,10 @@ if [[ " ${SCENARIOS[*]} " =~ " ${TEST} " ]]; then
   # Needed by pulp_container/tests/functional/api/test_flatpak.py:
   cmd_prefix dnf install -yq dbus-daemon flatpak
 fi
+
+# add the copied certificates from install.sh to the container's trusted certificates list
+if [[ "$TEST" = "azure" ]]; then
+  cmd_prefix sudo trust anchor /etc/pki/tls/cert.pem
+else
+  cmd_prefix sudo trust anchor /etc/pulp/certs/pulp_webserver.crt
+fi
