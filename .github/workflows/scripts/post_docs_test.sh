d7f0839fdee22f245bca56e96be964b2bab1fd35
pulpbot <pulp-infra@redhat.com>
2024-07-12 15:03:31 +0000

    Update CI files
    [noissue]

--- .github/workflows/scripts/post_docs_test.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-# make sure this script runs at the repo root
-cd "$(dirname "$(realpath -e "$0")")"/../../..
-
-export BASE_ADDR=https://pulp.example.com:443
-
-cd docs/_scripts/
-bash ./docs_check.sh
