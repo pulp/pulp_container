pulp/pulp-certguard

Update CI files

.github/workflows/scripts/publish_client_pypi.sh
@@ -14,18 +14,13 @@ cd "$(dirname "$(realpath -e "$0")")/../../.."
 
 VERSION="$1"
 
-if [[ -z "$VERSION" ]]; then
+if [[ -z "${VERSION}" ]]
+then
   echo "No version specified."
   exit 1
 fi
 
-RESPONSE="$(curl --write-out '%{http_code}' --silent --output /dev/null "https://pypi.org/project/pulp-certguard-client/$VERSION/")"
-
-if [ "$RESPONSE" == "200" ];
-then
-  echo "pulp-certguard client $VERSION has already been released. Skipping."
-else
-  twine upload -u __token__ -p "$PYPI_API_TOKEN" \
-  "dist/pulp_certguard_client-$VERSION-py3-none-any.whl" \
-  "dist/pulp_certguard-client-$VERSION.tar.gz"
-fi
+twine upload -u __token__ -p "${PYPI_API_TOKEN}" \
+"dist/pulp_certguard_client-${VERSION}-py3-none-any.whl" \
+"dist/pulp_certguard-client-${VERSION}.tar.gz" \
+;

.github/workflows/scripts/publish_plugin_pypi.sh
@@ -14,20 +14,13 @@ cd "$(dirname "$(realpath -e "$0")")"/../../..
 
 VERSION="$1"
 
-if [[ -z "$VERSION" ]]; then
+if [[ -z "${VERSION}" ]]
+then
   echo "No version specified."
   exit 1
 fi
 
-RESPONSE="$(curl --write-out '%{http_code}' --silent --output /dev/null "https://pypi.org/project/pulp-certguard/$VERSION/")"
-
-if [ "$RESPONSE" == "200" ];
-then
-  echo "pulp-certguard $VERSION has already been released. Skipping."
-  exit
-fi
-
-twine upload -u __token__ -p "$PYPI_API_TOKEN" \
-dist/pulp?certguard-"$VERSION"-py3-none-any.whl \
-dist/pulp?certguard-"$VERSION".tar.gz \
+twine upload -u __token__ -p "${PYPI_API_TOKEN}" \
+dist/pulp?certguard-"${VERSION}"-py3-none-any.whl \
+dist/pulp?certguard-"${VERSION}".tar.gz \
 ;

pulp/pulp_container

.github/workflows/scripts/publish_client_pypi.sh
@@ -14,18 +14,13 @@ cd "$(dirname "$(realpath -e "$0")")/../../.."
 
 VERSION="$1"
 
-if [[ -z "$VERSION" ]]; then
+if [[ -z "${VERSION}" ]]
+then
   echo "No version specified."
   exit 1
 fi
 
-RESPONSE="$(curl --write-out '%{http_code}' --silent --output /dev/null "https://pypi.org/project/pulp-container-client/$VERSION/")"
-
-if [ "$RESPONSE" == "200" ];
-then
-  echo "pulp_container client $VERSION has already been released. Skipping."
-else
-  twine upload -u __token__ -p "$PYPI_API_TOKEN" \
-  "dist/pulp_container_client-$VERSION-py3-none-any.whl" \
-  "dist/pulp_container-client-$VERSION.tar.gz"
-fi
+twine upload -u __token__ -p "${PYPI_API_TOKEN}" \
+"dist/pulp_container_client-${VERSION}-py3-none-any.whl" \
+"dist/pulp_container-client-${VERSION}.tar.gz" \
+;

.github/workflows/scripts/publish_plugin_pypi.sh
@@ -14,20 +14,13 @@ cd "$(dirname "$(realpath -e "$0")")"/../../..
 
 VERSION="$1"
 
-if [[ -z "$VERSION" ]]; then
+if [[ -z "${VERSION}" ]]
+then
   echo "No version specified."
   exit 1
 fi
 
-RESPONSE="$(curl --write-out '%{http_code}' --silent --output /dev/null "https://pypi.org/project/pulp-container/$VERSION/")"
-
-if [ "$RESPONSE" == "200" ];
-then
-  echo "pulp_container $VERSION has already been released. Skipping."
-  exit
-fi
-
-twine upload -u __token__ -p "$PYPI_API_TOKEN" \
-dist/pulp?container-"$VERSION"-py3-none-any.whl \
-dist/pulp?container-"$VERSION".tar.gz \
+twine upload -u __token__ -p "${PYPI_API_TOKEN}" \
+dist/pulp?container-"${VERSION}"-py3-none-any.whl \
+dist/pulp?container-"${VERSION}".tar.gz \
 ;