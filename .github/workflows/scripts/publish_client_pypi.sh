5b74249a9caf235760b03a8dcd82df933372da9c
pulpbot <pulp-infra@redhat.com>
2025-10-21 20:35:58 +0000

    Update CI files

--- .github/workflows/scripts/publish_client_pypi.sh
+++ .github/workflows/scripts/publish_client_pypi.sh
@@ -22,5 +22,5 @@ fi
 
 twine upload -u __token__ -p "${PYPI_API_TOKEN}" \
 "dist/pulp_container_client-${VERSION}-py3-none-any.whl" \
-"dist/pulp_container-client-${VERSION}.tar.gz" \
+"dist/pulp_container_client-${VERSION}.tar.gz" \
 ;
