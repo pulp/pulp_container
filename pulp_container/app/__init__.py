2de398c1a530f06dae7a75cc2522e8f0f0361935
pulpbot <pulp-infra@redhat.com>
2025-12-15 14:49:57 +0000

    Bump version: 2.26.5 → 2.26.6.dev

--- pulp_container/app/__init__.py
+++ pulp_container/app/__init__.py
@@ -6,7 +6,7 @@ class PulpContainerPluginAppConfig(PulpPluginAppConfig):
 
     name = "pulp_container.app"
     label = "container"
-    version = "2.26.5"
+    version = "2.26.6.dev"
     python_package_name = "pulp-container"
     domain_compatible = True
 
