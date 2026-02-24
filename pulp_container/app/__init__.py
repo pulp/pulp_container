e69cc810206829cf63361ac018dd4ba9ba1a0596
pulpbot <pulp-infra@redhat.com>
2026-02-24 19:49:49 +0000

    Release 2.19.7

--- pulp_container/app/__init__.py
+++ pulp_container/app/__init__.py
@@ -6,7 +6,7 @@ class PulpContainerPluginAppConfig(PulpPluginAppConfig):
 
     name = "pulp_container.app"
     label = "container"
-    version = "2.19.7.dev"
+    version = "2.19.7"
     python_package_name = "pulp-container"
 
     def ready(self):
