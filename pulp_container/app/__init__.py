3b7a5eb626e35711b1e2d285db98f63383ebb95d
Grant Gainey <ggainey@redhat.com>
2026-03-03 14:49:53 -0500

    Update several auto-created id fields/sequences to be BIGINT.
    fixes #2080.
    
    (cherry picked from commit 76528efe32e82c4f53f9cb6102e85d82b2528eac)

--- pulp_container/app/__init__.py
+++ pulp_container/app/__init__.py
@@ -1,4 +1,21 @@
 from pulpcore.plugin import PulpPluginAppConfig
+from django.db import connection
+from django.db.models.signals import post_migrate
+
+update_sequences_to_bigint = """
+ALTER TABLE container_blobmanifest ALTER COLUMN id TYPE bigint;
+ALTER TABLE container_manifestlistmanifest ALTER COLUMN id TYPE bigint;
+ALTER TABLE container_containerpushrepository_pending_blobs ALTER COLUMN id TYPE bigint;
+ALTER TABLE container_containerpushrepository_pending_manifests ALTER COLUMN id TYPE bigint;
+ALTER TABLE container_containerrepository_pending_manifests ALTER COLUMN id TYPE bigint;
+ALTER TABLE container_containerrepository_pending_blobs ALTER COLUMN id TYPE bigint;
+ALTER SEQUENCE container_blobmanifest_id_seq AS BIGINT;
+ALTER SEQUENCE container_manifestlistmanifest_id_seq AS BIGINT;
+ALTER SEQUENCE container_containerpushrepository_pending_blobs_id_seq AS BIGINT;
+ALTER SEQUENCE container_containerpushrepository_pending_manifests_id_seq AS BIGINT;
+ALTER SEQUENCE container_containerrepository_pending_blobs_id_seq AS BIGINT;
+ALTER SEQUENCE container_containerrepository_pending_manifests_id_seq AS BIGINT;
+"""
 
 
 class PulpContainerPluginAppConfig(PulpPluginAppConfig):
@@ -10,6 +27,14 @@ class PulpContainerPluginAppConfig(PulpPluginAppConfig):
     python_package_name = "pulp-container"
     domain_compatible = True
 
+    @staticmethod
+    def update_sequences(sender, **kwargs):
+        """Update database sequences to bigint type after migrations."""
+        with connection.cursor() as cursor:
+            cursor.execute(update_sequences_to_bigint)
+
     def ready(self):
         super().ready()
         from . import checks
+
+        post_migrate.connect(PulpContainerPluginAppConfig.update_sequences, sender=self)
