5fc36e7e0eb05c94930beb3798b400fb3c197176
pulpbot <pulp-infra@redhat.com>
2026-03-22 04:19:03 +0000

    Update CI files

--- pulp_container/tests/functional/api/test_build_images.py
+++ pulp_container/tests/functional/api/test_build_images.py
@@ -18,13 +18,11 @@ from pulpcore.client.pulp_container import (
 def containerfile_name():
     """A fixture for a basic container file used for building images."""
     with NamedTemporaryFile() as containerfile:
-        containerfile.write(
-            b"""FROM busybox:latest
+        containerfile.write(b"""FROM busybox:latest
 # Copy a file using COPY statement. Use the relative path specified in the 'artifacts' parameter.
 COPY foo/bar/example.txt /tmp/inside-image.txt
 # Print the content of the file when the container starts
-CMD ["cat", "/tmp/inside-image.txt"]"""
-        )
+CMD ["cat", "/tmp/inside-image.txt"]""")
         containerfile.flush()
         yield containerfile.name
 
