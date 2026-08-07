05c02a58bddf45e1bfce1cd0dd8c44fb7df657ed
Matthias Dellweg <mdellweg@redhat.com>
2026-08-07 16:38:57 +0200

    Remove dangerous unecessary call to strftime
    (cherry picked from commit 30b78439456108c8d2ef014f4ccc7aee951f7f13)

--- pulp_container/app/models.py
+++ pulp_container/app/models.py
@@ -2,7 +2,6 @@ import json
 import os
 import re
 import tempfile
-import time
 from gettext import gettext as _
 from logging import getLogger
 
@@ -797,8 +796,7 @@ INCOMPLETE_EXT = ".part"
 
 def generate_filename(instance, filename):
     """Method for generating upload file name"""
-    filename = os.path.join(instance.upload_dir, str(instance.pk) + INCOMPLETE_EXT)
-    return time.strftime(filename)
+    return os.path.join(instance.upload_dir, str(instance.pk) + INCOMPLETE_EXT)
 
 
 class Upload(CoreUpload):
